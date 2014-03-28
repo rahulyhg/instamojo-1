796018bf7e712ec9722a3e357665770bb89717e8
aniketpant <aniketpant@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 14:04:23 +0000

    Fix primary plugin file
    git-svn-id: https://plugins.svn.wordpress.org/instamojo/trunk@883569 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- instamojo-button.php
+++ instamojo-button.php
@@ -17,8 +17,6 @@ define('PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PLUGIN_NAME', 'INSTAMOJO');
 
-define('APPLICATION_ID', '');
-
 // Register the widget and the shortcodes
 register_activation_hook(__FILE__, 'instamojo_activation_hook');
 add_action('plugins_loaded', 'instamojo_plugin_loaded');
@@ -30,14 +28,14 @@ function instamojo_activation_hook()
 
 function instamojo_plugin_loaded()
 {
-  require_once(PLUGIN_DIR.'shortcode.php');
-  require_once(PLUGIN_DIR.'option.php');
+  include_once(PLUGIN_DIR.'shortcode.php');
+  include_once(PLUGIN_DIR.'option.php');
   add_action('widgets_init', 'instamojo_widgets_init');
 }
 
 function instamojo_widgets_init()
 {
-  require_once(PLUGIN_DIR.'widget.php');
+  include_once(PLUGIN_DIR.'widget.php');
   register_widget('instamojo_widget');
 }
 
