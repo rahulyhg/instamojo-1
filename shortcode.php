82a1c80e7aca0c85d4fd77f3fcfdcb6085bc714b
aniketpant <aniketpant@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-01 11:29:09 +0000

    Add shortcode
    git-svn-id: https://plugins.svn.wordpress.org/instamojo/trunk@885568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode.php
+++ shortcode.php
@@ -4,4 +4,25 @@
  * Shortcodes
  */
 
+add_shortcode('instamojo', 'instamojo_button');
+
+function instamojo_button($attributes, $content = null)
+{
+  extract(shortcode_atts(array(
+    'offer' => null,
+    'style' => 'none',
+    'text'  => 'Checkout with Instamojo'
+  ), $attributes));
+
+  $instamojo_credentials = get_option('instamojo_credentials');
+
+  wp_register_script('widgetjs', 'https://d2xwmjc4uy2hr5.cloudfront.net/im-embed/im-embed.min.js', 'jquery', null, true);
+  wp_enqueue_script('widgetjs');
+
+  if ($offer)
+  {
+    return '<a href="https://www.instamojo.com/'.$instamojo_credentials['username'].'/'.$offer.'/" rel="im-checkout" data-style="'.$style.'" data-text="'.$text.'"></a>';
+  }
+}
+
 ?>
